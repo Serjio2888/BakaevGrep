f1a9436d5f699898df3548762b6cb58869aec7db
Serjio2888 <kerchtown@yandex.ru>
2018-10-07 17:45:41 +0300

    end version

--- grep.py
+++ grep.py
@@ -6,85 +6,67 @@ import re
 def output(line):
     print(line)
 
-def simple_out(params, lines, i, realline):
+def context_(lines, params, i, nums):
 
-    if params.ignore_case:
-        lines[i]=lines[i].lower()
-    if not(params.invert):
-        if re.findall(params.pattern, lines[i]):
-            if params.line_number:
-                output(str(i+1)+':'+realline.rstrip())
-            if not(params.line_number):
-                output(realline.rstrip())
-    if params.invert:
-        if not(re.findall(params.pattern, lines[i])):
-            if params.line_number:
-                output(str(i+1)+':'+realline.rstrip())
-            if not(params.line_number):
-                output(realline.rstrip())
-
-        
-def findall(lines, params, string1, string2, i, nums):
-    
-    if params.before_context:
-        params.context = params.before_context
-    if params.after_context:
-        params.context = params.after_context
+    if (params.after_context and params.before_context):
+        params.context = params.after_context + params.before_context
+    else:
+        if params.before_context:
+            params.context = params.before_context
+        if params.after_context:
+            params.context = params.after_context
     for key in range(0, params.context+1,):
         if params.context:
             D = int(i-params.context/2+key)
-        if params.before_context:
+        if (params.after_context and params.before_context):
             D = int(i-params.before_context+key)
-        if params.after_context:
-            D = int(i+key)
+        else:
+            if params.before_context:
+                D = int(i-params.before_context+key)
+            if params.after_context:
+                D = int(i+key)
         if not (D) in nums:
-            if (D>=0):
+            if (D>=0) and (D>nums[0]):
                 if (D<len(lines)):
                     if params.line_number:
-                        if re.findall(params.pattern, lines[D]):
-                            output(str(D+1)+string1+lines[D].rstrip())
+                        if check_flags(lines[D], params):
+                            output(str(D+1)+':'+lines[D].rstrip())
                             nums.append(D)
                         else:
-                            output(str(D+1)+string2+lines[D].rstrip())
+                            output(str(D+1)+'-'+lines[D].rstrip())
                             nums.append(D)
                     else:
                         output(lines[D].rstrip())
                         nums.append(D)
+    return(nums)
 
-def context_(lines, params):
-
-    if params.context:
-        params.context *= 2
-        
-    nums =[]
-    for i in range(len(lines)):
-        if not(params.invert):
-            if re.findall(params.pattern, lines[i]):
-                findall(lines, params, ':', '-', i, nums)
-                                    
-        if params.invert:
-            if not(re.findall(params.pattern, lines[i])):
-                findall(lines, params, '-', ':', i, nums)
-    nums = []
+def check_flags(lines, params):
     
+    if not(params.invert):
+        if not(params.ignore_case):
+            return(re.findall(params.pattern, lines))
+        if params.ignore_case:
+            params.pattern = (params.pattern).lower()
+            lines = lines.lower()
+            return(re.findall(params.pattern, lines))
+    if (params.invert):
+        if not(params.ignore_case):
+            return not(re.findall(params.pattern, lines))
+        if params.ignore_case:
+            params.pattern = (params.pattern).lower()
+            lines = lines.lower()
+            return not(re.findall(params.pattern, lines))
 
-def count(lines, pattern, invert):
+def count(lines, params):
     """Считает количество строк,
        в которых есть совпадения с шаблоном.
-       Учитывает флаг 'invert'.
+       Учитывает флаги 'invert' и 'ignore_case'.
     """
-    k = -1
-    if not(invert):
-        for i in range(len(lines)):
-            if re.findall(pattern, lines[i]):
+    k = 0
+    for i in range(len(lines)):
+        if check_flags(lines[i], params):
                 k += 1
-        output(str(k))
-    if (invert):
-        for i in range(len(lines)):
-            if not(re.findall(pattern, lines[i])):
-                k += 1
-        output(str(k))
-
+    output(str(k))
             
 def grep(lines, params):
 
@@ -94,21 +76,30 @@ def grep(lines, params):
     params.pattern = re.sub('\?',r'.',params.pattern)
     
     if params.count:
-        return(count(lines, params.pattern, params.invert))
+        return(count(lines, params))
 
-    if not(params.context or params.before_context or params.after_context):
-        for i in range(len(lines)):
-            realline = str(lines[i])
-            simple_out(params, lines, i, realline)
-            
+    nums = [-1]
+
     if params.context:
-        context_(lines, params)
-        
-    if params.before_context:
-        context_(lines, params)
+        params.context *= 2
+                    
+    for i in range(len(lines)):
+        if check_flags(lines[i], params):
+            if not(params.context or params.before_context or params.after_context):
+                if not(params.line_number):
+                    output(lines[i].rstrip())
+                if params.line_number:
+                    output(str(i+1)+':'+lines[i].rstrip())
+            else:
+                if params.context:
+                    nums = context_(lines, params, i, nums)
+                else:
+                    if params.before_context:
+                        nums = context_(lines, params, i, nums)
+                    if params.after_context:
+                        nums = context_(lines, params, i, nums)
+                            
 
-    if params.after_context:
-        context_(lines, params)
 
 def parse_args(args):
 
